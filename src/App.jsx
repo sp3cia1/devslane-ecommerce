487ba95e0054b4ae379650e0753e31cbdf87f2e3
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-07-08 07:05:28 +0530

    Complete assignment 55.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from './components/Header'
 import Container from './components/Container'
 import Footer from './components/Footer'
+import ProductDetail from './components/ProductDetail';
+
 
 function App() {
   return (
     <div className="bg-gray-100 flex flex-col min-h-screen">
        <Header/>
-       <Container/>
+       {/* <Container/> */}
+       <ProductDetail/>
        <Footer/> 
     </div>
   );

9eea90287098f3e556bec1fac766f43396094503
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2025-07-15 12:51:03 +0530

    Refactor forms to use withFormik HOC and create reusable Input component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,75 +1,64 @@
-import { useFormik } from "formik";
+import { withFormik } from "formik";
 import { Link } from "react-router";
 import * as Yup from "yup";
+import Input from "./Input";
+
+function loginApi(values) {
+  console.log("Sending Data", values.username, values.password);
+}
 
 const LoginSchema = Yup.object().shape({
   username: Yup.string().min(4).required(),
   password: Yup.string().min(8).required(),
 });
 
-export default function LoginForm() {
-  function loginApi(values) {
-    console.log("Sending Data", values.username, values.password);
-  }
+const initialValues = {
+  username: "",
+  password: "",
+};
 
-  const {
-    handleSubmit,
-    values,
-    handleChange,
-    touched,
-    handleBlur,
-    dirty,
-    errors,
-    isValid
-  } = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-    },
-    validationSchema: LoginSchema,
-    onSubmit: loginApi,
-  });
-
+function LoginForm({
+  values,
+  touched,
+  errors,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  dirty,
+  isValid
+}) {
   return (
     <div>
       <form onSubmit={handleSubmit} className="flex flex-col">
         <div className="mb-4">
-          <label htmlFor="username" className="sr-only">
-            username
-          </label>
-          <input
+          <Input
             id="username"
+            label="Username"
+            name="username"
+            values={values.username}
+            error={errors.username}
             placeholder="USERNAME"
-            value={values.username}
+            touched={touched.username}
             onChange={handleChange}
             onBlur={handleBlur}
-            name="username"
-            type="text"
             required
-            className="border rounded-md border-white text-white p-4 text-sm w-80 bg-transparent placeholder-gray-300"
+            type="text"
           />
-          {touched.username && errors.username && values.username && (
-            <div className="text-red-300 text-sm mt-1">{errors.username}</div>
-          )}
         </div>
         <div className="">
-          <label htmlFor="password" className="sr-only">
-            password
-          </label>
-          <input
+          <Input
             id="password"
+            label="Password"
+            name="password"
+            values={values.password}
+            error={errors.password}
             placeholder="PASSWORD"
-            value={values.password}
+            touched={touched.password}
             onChange={handleChange}
             onBlur={handleBlur}
-            name="password"
-            type="password"
             required
-            className="border rounded-md border-white text-white p-4 text-sm w-80 bg-transparent placeholder-gray-300"
+            type="password"
           />
-          {touched.password && errors.password && values.password && (
-            <div className="text-red-300 text-sm mt-1">{errors.password}</div>
-          )}
         </div>
 
         <div className="mt-4 flex flex-col gap-1">
@@ -84,16 +73,31 @@ export default function LoginForm() {
           >
             LOGIN
           </button>
-          <Link to="/auth/forgot-password" className="text-white self-end hover:underline">
+          <Link
+            to="/auth/forgot-password"
+            className="text-white self-end hover:underline"
+          >
             Forgot Password?
           </Link>
         </div>
-        
       </form>
       <p className="text-white text-center text-lg mt-4">
-        Dont have an account? 
-        <Link to="/auth/signup" className="font-bold hover:underline"> Sign Up</Link>
+        Dont have an account?
+        <Link to="/auth/signup" className="font-bold hover:underline">
+          {" "}
+          Sign Up
+        </Link>
       </p>
     </div>
   );
 }
+
+const formikHOC = withFormik({
+  validationSchema: LoginSchema,
+  initialValues: initialValues,
+  handleSubmit: loginApi,
+});
+
+const LoginFormWithFormikHOC = formikHOC(LoginForm);
+
+export default LoginFormWithFormikHOC;
